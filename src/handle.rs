855d7057e6e2d14051c320fabf5087eb10368ef7
Sawawada8 <sawadadamskz7575@gmail.com>
2023-02-05 00:13:06 +0900

    ci setup

--- src/handle.rs
+++ src/handle.rs
@@ -2,3 +2,17 @@
 pub fn handle() {
     println!("handle");
 }
+
+pub fn demo() -> i32 {
+    2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn demo_test() {
+        assert_eq!(2, demo());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,3 +4,11 @@ fn main() {
     println!("Hello, world! main");
     handle::handle();
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn demo_test() {
+        assert_eq!(4, 2 + 2);
+    }
+}
