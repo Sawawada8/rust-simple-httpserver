5678d5d945629cc22d8cf15142c09145c790a9f1
Sawawada8 <sawadadamskz7575@gmail.com>
2023-02-05 02:00:54 +0900

    wip

--- examples/client.rs
+++ examples/client.rs
@@ -1,27 +1,66 @@
+use std::io::prelude::*;
+
 use std::net::{TcpStream};
+use std::io::{BufReader,BufRead};
 
 const SERVER: &str = "0.0.0.0:8082";
 
 fn main() -> std::io::Result<()> {
     println!("===============");
-    println!("client start");
+    println!("CLIENT start");
     println!("===============\n");
 
     // 接続する
     println!("connect serve !");
     // 成功すればそのまま処理が続く、失敗すれば Err返す
-    let stream = TcpStream::connect(SERVER)?;
+    let mut stream = TcpStream::connect(SERVER)?;
     println!("Stream: {:?}", stream);
 
-    // こんな感じにもかける
-    // match TcpStream::connect(SERVER) {
-    //     Ok(result) => {println!("Ok: {:?}", result);},
-    //     Err(err) => {println!("Err: {:?}", err)}
-    // };
+    // write
+    write(&mut stream);
+
+
+    // read
+    // readが思ったようにできないが、serverから返していいないから？
+    read(&mut stream);
+    // read_byte(&mut stream);
+
 
 
     println!("\n===============");
     println!("client end");
     println!("===============");
     Ok(())
+}
+
+fn write(stream: &mut TcpStream) -> &mut TcpStream {
+    let request_text = String::from("request text").into_bytes();
+    if let Ok(result) = stream.write(&request_text) {
+        println!("write OK : {:?}", result);
+        stream
+    } else {
+        stream
+    }
+}
+
+fn read(stream: &mut TcpStream) {
+    // stream から response を読めるようにする
+    let mut recv_response = String::new();
+    let mut reader = BufReader::new(stream);
+
+    // response 読んでいく
+    if let Ok(v) = reader.read_line(&mut recv_response) {
+        println!("v = {}", v);
+        if v > 0 {
+            //レスポンスを表示
+            println!("response: {}",recv_response);
+        }
+    }
+}
+
+fn read_byte(stream: &mut TcpStream) {
+    let reader = BufReader::new(stream);
+    let bytes = reader.buffer();
+    let converted: String = String::from_utf8(bytes.to_vec()).unwrap();
+    println!("response: {:?}", converted);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{net::{TcpListener}, io::Write};
+use std::{net::{TcpListener}, io::{Write, BufRead, BufReader, Read}};
 
 mod handle;
 
@@ -7,7 +7,7 @@ const HOST: &str = "0.0.0.0:8082";
 
 fn main() -> std::io::Result<()> {
     println!("=====================");
-    println!("main start");
+    println!("SERVER start");
     println!("=====================");
     handle::handle();
 
@@ -19,10 +19,26 @@ fn main() -> std::io::Result<()> {
         println!("accept !!!");
         println!("-----------------");
 
-        let mut socket = stream?;
+        let mut socket = stream?.try_clone().unwrap();
+
+        // stream から request を読み込む
+        let mut request = String::new();
+        let mut reader = BufReader::new(&socket);
+
+        if let Ok(v) = reader.read_line(&mut request) {
+            println!("v = {}", v);
+            if v > 0 {
+                // リクエストを表示
+                println!("Request: {}",request);
+            }
+        } else {
+            println!("no response");
+        }
+
         // 受け付けた socket に書き込む
-        let response_text = String::from("hello").into_bytes();
-        socket.write(&response_text);
+        let response_text = String::from("hello world").into_bytes();
+        // let mut res_socket = stream;
+        socket.write(&response_text)?;
     }
 
     Ok(())
