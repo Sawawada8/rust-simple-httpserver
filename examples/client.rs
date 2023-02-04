3c51ab4014bcfd3b2f38308242f99e9390642a1b
Sawawada8 <sawadadamskz7575@gmail.com>
2023-02-05 00:36:36 +0900

    client 実装

--- examples/client.rs
+++ examples/client.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("hello examples client");
+use std::net::{TcpStream};
+
+const SERVER: &str = "0.0.0.0:8082";
+
+fn main() -> std::io::Result<()> {
+    println!("===============");
+    println!("client start");
+    println!("===============\n");
+
+    // 接続する
+    println!("connect serve !");
+    // 成功すればそのまま処理が続く、失敗すれば Err返す
+    let stream = TcpStream::connect(SERVER)?;
+    println!("Stream: {:?}", stream);
+
+    // こんな感じにもかける
+    // match TcpStream::connect(SERVER) {
+    //     Ok(result) => {println!("Ok: {:?}", result);},
+    //     Err(err) => {println!("Err: {:?}", err)}
+    // };
+
+
+    println!("\n===============");
+    println!("client end");
+    println!("===============");
+    Ok(())
 }
