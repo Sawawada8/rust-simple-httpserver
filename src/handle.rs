58a3aee220c673ac6714eb636356898cef14c082
Sawawada8 <sawadadamskz7575@gmail.com>
2023-02-05 14:46:55 +0900

    マルチスレッドで動くように

--- src/handle.rs
+++ src/handle.rs
@@ -1,19 +1,49 @@
+use std::io::{Write, BufRead, BufReader, Read};
+use std::fmt::format;
+use std::fs::File;
+use std::net::TcpStream;
+use std::thread;
+use std::time::{Duration};
 
-pub fn handle() {
-    demo();
-    println!("handle");
+
+pub fn handle(mut socket: TcpStream) {
+        // stream から request を読み込む
+        let mut b = [0; 1024];
+
+        socket.read(&mut b).unwrap();
+
+        let sleep = b"GET /sleep HTTP/1.1\r\n";
+        if b.starts_with(sleep) {
+            println!("call sleep");
+            thread::sleep(
+                Duration::from_secs(5)
+            );
+        }
+        println!("REQUEST: {:?}", String::from_utf8_lossy(&b[..]));
+
+
+        // 受け付けた socket に書き込む
+        let mut file = File::open("i.html").unwrap();
+
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+
+        let response_text = format!(
+            "HTTP/1.1 200 OK\r\n\r\n{}",
+            contents
+        );
+        // let mut res_socket = stream;
+        socket.write(response_text.as_bytes()).unwrap();
+        socket.flush().unwrap();
 }
 
-pub fn demo() -> i32 {
-    2
-}
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    // use super::*;
 
     #[test]
     fn demo_test() {
-        assert_eq!(2, demo());
+        assert_eq!(2, 2);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{net::{TcpListener}, io::{Write, BufRead, BufReader, Read}, fmt::format, fs::File};
+use std::{net::{TcpListener}, io::{Write, BufRead, BufReader, Read}, fmt::format, fs::File, time::Duration, thread};
 
 mod handle;
 
@@ -9,7 +9,6 @@ fn main() -> std::io::Result<()> {
     println!("=====================");
     println!("SERVER start");
     println!("=====================");
-    handle::handle();
 
     let listener = TcpListener::bind(HOST)?;
 
@@ -20,31 +19,18 @@ fn main() -> std::io::Result<()> {
         println!("-----------------");
 
         // let mut socket = stream?.try_clone().unwrap();
-        let mut socket = stream.unwrap();
+        let socket = stream.unwrap();
 
-        // stream から request を読み込む
-        let mut b = [0; 1024];
-        socket.read(&mut b).unwrap();
-        println!("REQUEST: {:?}", String::from_utf8_lossy(&b[..]));
-
-
-        // 受け付けた socket に書き込む
-        let mut file = File::open("i.html").unwrap();
-
-        let mut contents = String::new();
-        file.read_to_string(&mut contents).unwrap();
-
-        let response_text = format!(
-            "HTTP/1.1 200 OK\r\n\r\n{}",
-            contents
-        );
-        // let mut res_socket = stream;
-        socket.write(response_text.as_bytes())?;
-        socket.flush().unwrap();
+        // socket は今後使わないので、そのまま渡して大丈夫
+        // multi thred で処理
+        thread::spawn(|| {
+            handle::handle(socket);
+        });
     }
 
     Ok(())
 }
+
 
 #[cfg(test)]
 mod tests {
